eca5746e64de4536737d7fd679ff4136e9205df8
Lood800 <lodewicus@gmail.com>
2020-03-21 10:53:47 -0400

    first push

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+// webpack.config.js
+const webpack = require('webpack')
+const dotenv = require('dotenv')
+
+module.exports = {
+  //...
+  plugins: [
+    // ...
+    new webpack.DefinePlugin({
+       'process.env': JSON.stringify(dotenv.config().parsed) // it will automatically pick up key values from .env file
+    })
+    // ...
+  ]
+  //...
+}
